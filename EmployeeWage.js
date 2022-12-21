53c4a10b9e08fb5a3a7ea2a9f7a78cb42a84a8cb
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-12-21 22:41:54 +0530

    Added: UC5_Calculating employee monthly wage using while loop.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -7,7 +7,9 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 const NUM_OF_WORKING_DAYS=20;
-
+const MAXIMUM_HRS_MONTH=100;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
 function getWorkingHours(empCheck){ 
 switch(empCheck){
     case IS_PART_TIME:
@@ -22,11 +24,12 @@ switch(empCheck){
           //console.log("Employee is Absent")
     }
 }
-   let empHrs=0;
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
-    let empCheck =Math.floor(Math.random() *10)%3;
-    empHrs+=getWorkingHours(empCheck);
+   
+while(totalEmpHrs<=MAXIMUM_HRS_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+      totalWorkingDays++;
+      let empCheck =Math.floor(Math.random() *10)%3;
+      totalEmpHrs+=getWorkingHours(empCheck);
 }
    
-    let empWage= empHrs*WAGE_PER_HOUR;
-    console.log("Total hours:---" +empHrs+ " Emp wage:--" +empWage);
+    let empWage= totalEmpHrs*WAGE_PER_HOUR;
+    console.log("Total Days:- " +totalWorkingDays+ " Total hours :-- " +totalEmpHrs+ " Emp wage:--" +empWage);
