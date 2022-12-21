fb48f120526df8d41a39ed7f0c21a239128d3a9a
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-12-21 21:08:36 +0530

    Added: UC1_Employee present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,12 @@
 console.log("welcome to employee wage program.")
+//UC1 employee is present or absent
+
+const IS_ABSENT=0
+
+let empCheck =Math.floor(Math.random() *10)%2;
+if(empCheck==IS_ABSENT){
+    console.log("Employee is Absent");
+    return;
+}else{
+    console.log("Employee is Present");
+}
